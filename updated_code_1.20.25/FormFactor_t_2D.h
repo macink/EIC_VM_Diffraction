8087b69bca2afd198b40d34d5cade8540d76cefd
macink <70175883+macink@users.noreply.github.com>
2025-01-21 15:02:32 -0500

    Add files via upload
    updated code

---
+++ updated_code_1.20.25/FormFactor_t_2D.h
@@ -0,0 +1,136 @@
+#ifndef FormFactor_t_2D_H
+#define FormFactor_t_2D_H
+
+#include "EICvalueconst.h"
+
+using namespace std;
+
+/********************************************** 
+ 
+ * Form Factor 2D: F(tx,ty) [-]
+ 
+ * Parameters:
+    Vo: Nuclear density (hard sphere approximation) [fm^-3]
+    A: Atomic mass number
+    R: Radius of nucleus [fm]
+    a0: Range of Yukawa potential [fm]
+ * Variables:
+    t: Four-momentum transfer squared >> t = tx+ty = q^2 [GeV^2]
+
+**********************************************/
+class FormFactor_t_2D
+{
+public:
+    FormFactor_t_2D(double A_init, double Vo_init, double R_init, double a0_init, double tx_min, double tx_max,
+        double ty_min, double ty_max, double bins, double x_min, double x_max, double y_min, double y_max): A(A_init), Vo(Vo_init), R(R_init), a0(a0_init)
+    {
+        // Initialize |F(tx,ty)|^2 TF2
+        FF2DtSquared = new TF2("Form Factor", calc_FormFactor_2t2D, tx_min, tx_max, ty_min, ty_max, 4);
+        FF2DtSquared->SetParameters(A, Vo, R, a0);
+
+        // Initialize |F(tx,ty)^2| 2D histogram
+        hist2D = new TH2D("", "|F(t_{x},t_{y})^{2}| 2D Histogram", bins, tx_min, tx_max, bins, ty_min, ty_max);
+        hist2D->Sumw2();
+        double step = tx_max/bins;
+        for (int i=0;i<=bins;i++) 
+        {
+            double tx = (i+1)*step;
+            for (int j=0;j<=bins;j++) 
+            {
+                double ty = (j+1)*step;
+                double val = calc_FF_2t2D(tx, ty);
+                hist2D->SetBinContent(i+1, j+1, val);
+            }
+        }
+
+        // Initialize TF2 for transform: |F(tx,ty)|^2 -> G(x,y)
+        transform_TF2 = new TF2("Fourier-Bessel Transformation: |F(t_{x},t_{y})|^{2} -> G(x,y)", [this, tx_min, tx_max, ty_min, ty_max] (double *var, double *par)
+        {
+            double x = var[0], y = var[1];
+            TF2 trans_integral("trans_integral", trueG1_integrand_dq_2D, tx_min, tx_max, ty_min, ty_max, 2);
+            trans_integral.SetParameters(x, y);
+            return trans_integral.Integral(tx_min, tx_max, ty_min, ty_max, 1e-12);
+        }, x_min, x_max, y_min, y_max, 0);  
+
+        // Initialize hist for transform: |F(tx,ty)|^2 -> G(x,y)
+        transform_hist = new TH2D("hankelTransformFF", "Transform Form Factor", bins, x_min, x_max, bins, y_min, y_max);
+        transform_hist->Sumw2();
+        double step2 = x_max/bins;
+        for(int i=0;i<bins;i++)
+        {
+            double x = (i+1)*step2;
+            for(int j=0;j<bins;j++)
+            {
+                double y = (j+1)*step2;
+                double val = trueG1_integral_dq_2D(x, y);
+                transform_hist->SetBinContent(i+1, j+1, val);
+            }
+        }
+    }
+
+
+    // Calculate Form Factor and G integrand
+    static double calc_FormFactor_2t2D(double *var, double *par) // |F(tx,ty)|^2
+    {
+        double A = par[0], Vo = par[1], R = par[2], a0 = par[3];
+        double tx = var[0], ty = var[1];
+        double t = tx+ty;
+        double q = sqrt(t);
+	    const double arg1 = q*R / hbarc;  
+	    const double arg2 = hbarc / q;
+	    const double arg3  = (sin(arg1)-arg1*cos(arg1)) * 4*pi*Vo * arg2*arg2*arg2 / A; 
+	    double result = arg3 / (1. + (a0*a0 * q*q/(hbarc*hbarc)));  // [-]
+        return result*result;
+    }
+    static double calc_FF_2t2D(double tx, double ty) // |F(qx,qy)|^2 [-]
+    {
+        double t = tx+ty, q = sqrt(t);
+        double A = 197, Vo = 2.12, R = 6.38, a0 = 0.7;
+	    const double arg1 = q*R / hbarc;  
+	    const double arg2 = hbarc / q;
+	    const double arg3  = (sin(arg1)-arg1*cos(arg1)) * 4*pi*Vo * arg2*arg2*arg2 / A; 
+        double result = arg3 / (1. + (a0*a0 * q*q/(hbarc*hbarc))); 
+	    return result*result; 
+    }
+    // Calculations for transformation |F(tx,ty)|^2 -> G(x,y)
+    static double calc_FF(double qx, double qy) // |F(qx,qy)|^2 [-]
+    {
+        double A = 197, Vo = 2.12, R = 6.38, a0 = 0.7;
+        double q = sqrt(qx*qx+qy*qy);
+	    const double arg1 = q*R / hbarc;  
+	    const double arg2 = hbarc / q;
+	    const double arg3  = (sin(arg1)-arg1*cos(arg1)) * 4*pi*Vo * arg2*arg2*arg2 / A; 
+        double result = arg3 / (1. + (a0*a0 * q*q/(hbarc*hbarc))); 
+	    return result; 
+    }
+    static double trueG1_integrand_dq_2D(double *var, double *par) 
+    {
+        double x = par[0], y = par[1];
+        double qx = var[0], qy = var[1];
+        double r = sqrt(x*x+y*y), q = sqrt(qx*qx+qy*qy);
+        return 2*pi*sqrt(2*pi/r)*sqrt(2*hbarc/(pi*q*r))*sin(q*r/hbarc)*calc_FF(qx,qy)*sqrt(q)*q*1/hbarc*1/hbarc*1/sqrt(hbarc); //[1/fm^3GeV^2] 
+    }
+    static double trueG1_integral_dq_2D(double x, double y)
+    {
+        double qq_min = 0, qq_max = 5;
+        TF1 *f = new TF1("f", trueG1_integrand_dq_2D, qq_min, qq_max, 2);  
+        f->SetParameters(x, y);
+        return f->Integral(qq_min, qq_max, 1e-12); //[1/fm^3]
+    }
+    
+
+    // Functions
+    TF2 *getFormFactort2_2D() const {return FF2DtSquared;}
+    TH2D *getFormFactort_hist() const {return hist2D;}
+    TF2 *getTransformed_TF2() const {return transform_TF2;}
+    TH2D *getTransformed_hist() const {return transform_hist;}
+
+
+private:
+    double A, Vo, R, a0;
+    TF2 *FF2DtSquared, *transform_TF2;
+    TH2D *hist2D, *transform_hist;
+};
+
+
+#endif
---
+++ updated_code_1.20.25/FormFactor_transform_resolution_add_wedge_2D.h
@@ -0,0 +1,127 @@
+#ifndef FormFactor_transform_resolution_add_wedge_2D_H
+#define FormFactor_transform_resolution_add_wedge_2D_H
+
+#include "EICvalueconst.h"
+
+using namespace std;
+
+
+
+
+/********************************************** 
+ 
+ * Form Factor with detector resolution and wedge cut 2D: |F(t_x,t_y)|^2
+ 
+ * Parameters:
+    Vo: Nuclear density (hard sphere approximation) [fm^-3]
+    A: Atomic mass number
+    R: Radius of nucleus [fm]
+    a0: Range of Yukawa potential [fm]
+ * Variables:
+    q: Transverse momentum >> q = sqrt(qx^2+qy^2) [GeV]
+    t: Four-momentum transfer squared >> t = tx+ty = q^2 [GeV^2]
+
+**********************************************/
+class FormFactor_transform_resolution_add_wedge_2D
+{
+public:
+ // Calculations to add resolution to form factor
+    static double calcFF(double q)
+    {
+        double A = 197, Vo = 2.12, R = 6.38, a0 = 0.7;
+	    const double arg1 = q*R / hbarc;  
+	    const double arg2 = hbarc / q;
+	    const double arg3  = (sin(arg1)-arg1*cos(arg1)) * 4*pi*Vo * arg2*arg2*arg2 / A; 
+	    double result = 1/(2*pi)*arg3 / (1. + (a0*a0 * q*q/(hbarc*hbarc))); 
+        return result*result; // [-]
+    }
+    static double guassian(double *x, double *par)
+    {
+        double qy = par[0], qx_prime = par[1], sigma = par[2];
+        double qx = x[0];  // integration variable
+        double q = sqrt(qx*qx+qy*qy);
+	    return calcFF(q)*exp(-(qx-qx_prime)*(qx-qx_prime)/(2*sigma*sigma));
+    }
+    // Integral over qx, new 2D form factor with smearing
+    static double FF_wRes(double qy, double qx_prime, double sigma)
+    {
+    double qx_min = 0;
+    double qx_max = 5;
+    TF1 *f = new TF1("f",guassian,qx_min,qx_max,3);  
+    f->SetParameters(qy,qx_prime,sigma);
+    return f->Integral(qx_min,qx_max,1e-12); // F(qx',qy) [GeV]
+}
+
+// Integrand for wedge cut
+static double testwedge_calcFF2(double *x, double *par)
+{
+    double q_prime = par[0], sigma = par[1];
+    // integration variable
+    double phi = x[0];
+    double qx_prime = q_prime*sin(phi);
+    double qy = q_prime*cos(phi);
+    // call form factor with resolution
+	return FF_wRes(qy,qx_prime,sigma)*FF_wRes(qy,qx_prime,sigma); // [GeV^2]
+}
+
+// Integral to integrate over theta
+static double testwedge_FF2_inte_2D(double q_prime, double phi_min, double phi_max, double sigma)
+{
+    //double theta_min = 0;
+    //double theta_max = pi/9;
+    TF1 *f = new TF1("f",testwedge_calcFF2,phi_min,phi_max,2);  
+    f->SetParameters(q_prime,sigma);
+    return f->Integral(phi_min,phi_max,1e-12); //[GeV]
+}
+
+
+
+
+    // Integrand: |F(t)|^2 (wedge, no resolution)->G(x,y) 
+static double G_integrand(double *x, double *par) 
+{
+    double xx = par[0];
+    double y = par[1],  phi_min = par[2], phi_max = par[3], sigma = par[4];
+    double r = sqrt(xx*xx + y*y);
+    // integration variable
+    double qx_prime = x[0], qy = x[1];
+    double q_prime = sqrt(qx_prime*qx_prime+qy*qy);
+    return sqrt(2*pi/(r))*sqrt(2*hbarc/(pi*q_prime*r))*sin(q_prime*r/hbarc)*testwedge_FF2_inte_2D(q_prime,phi_min,phi_max,sigma)*sqrt(q_prime)*1/hbarc*1/hbarc*1/sqrt(hbarc); // [GeV^-1 fm^-3]
+}
+
+// Integral for G(x,y)
+static double G_integral_dq(double x, double y,double phi_min, double phi_max, double sigma)
+{
+    double qx_prime_min = 0., qx_prime_max = 1., qy_min = 0., qy_max = 1.;
+    TF2 *f = new TF2("f",G_integrand,qx_prime_min,qx_prime_max,qy_min,qy_max,5);  
+    f->SetParameters(x,y,phi_min,phi_max,sigma);
+    return f->Integral(qx_prime_min,qx_prime_max,qy_min,qy_max,1e-12); //[1/fm^3]
+}
+
+
+       
+        TH2D *hist2(const char *name, const char *title, double bins, double xmin, double xmax, double ymin, double ymax, double phi_min, double phi_max, double sigma)
+        {
+            TH2D *hist2D = new TH2D(name, title, bins, xmin, xmax, bins, ymin, ymax);
+            hist2D->Sumw2();
+            double step = xmax/bins;
+            for(int i=0;i<bins;i++)
+            {
+                double x = (i+1)*step;
+                for(int j=0;j<bins;j++)
+                {
+                    double y = (j+1)*step;
+                    double val = G_integral_dq(x, y, phi_min, phi_max, sigma);
+                    hist2D->SetBinContent(i+1, j+1, val);
+                }
+                cout << "x: " << x << "\n";
+            }
+            return hist2D;
+        }
+
+
+    
+};
+
+#endif
+
