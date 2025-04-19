de232007a9be524e21adc547ba56e60d570e45b4
macink <70175883+macink@users.noreply.github.com>
2025-04-19 10:06:51 -0400

    Update plot_2d_combined_wCUT.C

--- EICreconOutputReader/macros/plot_2d_combined_wCUT.C
+++ EICreconOutputReader/macros/plot_2d_combined_wCUT.C
@@ -8,6 +8,7 @@ void plot_2d_combined_wCUT()
 	TString filename = "/home/macink/miniconda3/envs/bnl_research/macros/eic/EICreconOutputReader/output/combined_2dhistograms_wCUT.root";
 	TFile* file = new TFile(filename);
 	TString vm_label="#phi";
+	TString angle = "#pi/12";
 	TString daug_label="K^{+}K^{-}";
 	if(filename=="jpsi") {vm_label="J/#psi";daug_label="e^{+}e^{-}";}
 	//t distribution
@@ -17,6 +18,13 @@ void plot_2d_combined_wCUT()
 	gPad->SetLogz(1);
 	
 	h_t_REC_2d_wCUT->Draw();
+
+	TLatex* r46 = new TLatex(0.6, 0.9, "#theta_{max}= "+angle);
+	r46->SetNDC();
+	r46->SetTextSize(15);
+	r46->SetTextFont(43);
+	r46->SetTextColor(kBlack);
+	r46->Draw("same");
 
 	c1->Print("/home/macink/miniconda3/envs/bnl_research/macros/eic/EICreconOutputReader/figures/combined/combined_2d_wCUT.pdf");
 }
